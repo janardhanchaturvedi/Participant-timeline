49e6d0bf80352240bcc3927945da09572f1c13e3
Janardhan <jan.cj84@gmail.com>
2024-11-26 22:56:26 +0530

    Added few changes

--- src/Component/Timeline/index.jsx
+++ src/Component/Timeline/index.jsx
@@ -1,11 +1,14 @@
 import React from "react";
 import clipboard from "./../../assets/clipboard.svg";
+import meetingData from "./../../constants/meetingdata.json";
 export default function Timeline() {
+  console.log("meetingData", meetingData);
+  
   return (
     <div className="max-w-[1280px]">
       <div className=" h-[100vh] w-[100vw] flex justify-center items-center">
         <section className="bg-timeLineBgColor h-[451px] w-[1000px] rounded-xl ">
-          <div className="flex justify-between items-center p-3 w-full bg-timeLineBg rounded-lg">
+          <div className="flex justify-between items-center p-3 w-full bg-timeLineBg rounded-lg  ">
             <div className="flex justify-center items-center gap-2">
               <span className="">
                 <img src={clipboard} alt="clipboard" />
@@ -18,6 +21,7 @@ export default function Timeline() {
               Show participant timeline
             </div>
           </div>
+          <div className="w-full h-[1px] bg-[#cecece]"></div>
         </section>
       </div>
     </div>
