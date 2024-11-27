a25ede54b083ee0cc45bd06e9fedbec8050603f5
Janardhan <jan.cj84@gmail.com>
2024-11-27 22:41:41 +0530

    Added some changes

--- src/Component/Timeline/index.jsx
+++ src/Component/Timeline/index.jsx
@@ -1,9 +1,36 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import clipboard from "./../../assets/clipboard.svg";
 import meetingData from "./../../constants/meetingdata.json";
+import dayjs from "dayjs";
+import utc from "dayjs/plugin/utc";
 export default function Timeline() {
-  console.log("meetingData", meetingData);
-  
+  const [timeMeasurement, settimeMeasurement] = useState({
+    hourDifference: 0,
+    minutesDifference: 0,
+    timeStep: 0,
+  });
+  const startTime = meetingData?.start;
+  const endTime = meetingData?.end;
+  const startHour = dayjs(startTime).format("HH");
+  const startMinute = dayjs(startTime).format("mm");
+
+  const endHour = dayjs(endTime).format("HH");
+  const endMinute = dayjs(endTime).format("mm");
+
+
+  useEffect(() => {
+    if (startHour === endHour) {
+      const minutesDifference = endMinute - startMinute;
+      const minutesSteps = Math.round(minutesDifference / 60);
+      settimeMeasurement({
+        ...timeMeasurement,
+        minutesDifference: minutesDifference,
+        timeStep: minutesSteps,
+      });
+    }
+  }, []);
+  const timeSteps = new Array(10).fill(0);
+
   return (
     <div className="max-w-[1280px]">
       <div className=" h-[100vh] w-[100vw] flex justify-center items-center">
@@ -22,6 +49,19 @@ export default function Timeline() {
             </div>
           </div>
           <div className="w-full h-[1px] bg-[#cecece]"></div>
+          {/* Time series */}
+          <div>
+            {timeMeasurement.minutesDifference &&
+              timeSteps.map((items, index) => {
+                const minutes =
+                  startMinute + (index + 1) * timeMeasurement?.timeStep;
+                return (
+                  <>
+                    <div className="text-gray-500">{`${startHour} : ${minutes}`}</div>
+                  </>
+                );
+              })}
+          </div>
         </section>
       </div>
     </div>
